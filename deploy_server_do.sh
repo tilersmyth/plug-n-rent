fc61005c751ec0aac3be75ca8db35a4693551085
Tyler Smith <tyler.smith.la@gmail.com>
2018-06-25 20:34:16 -0400

    Updated deploy script

--- deploy_server_do.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-yarn build:server
-docker build -t tilersmyth/plugnrent:latest .
-docker push tilersmyth/plugnrent:latest
-ssh root@206.81.6.87 "docker pull tilersmyth/plugnrent:latest && docker tag tilersmyth/plugnrent:latest dokku/plugnrent:latest && dokku/plugnrent:latest && dokku tags:deploy plugnrent latest"
