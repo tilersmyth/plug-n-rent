9ce346f081a4541cbf608cece051f76d389c1ca6
Tyler Smith <tyler.smith.la@gmail.com>
2018-07-10 21:02:20 -0400

    Switch deploy to Heroku

--- deploy_script_do.sh
+++ deploy_script_do.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 yarn build:server
-docker build -t tilersmyth/plugnrent:latest .
-docker push tilersmyth/plugnrent:latest
-ssh root@206.81.6.87 "docker pull tilersmyth/plugnrent:latest && docker tag tilersmyth/plugnrent:latest dokku/plugnrent:latest && dokku tags:deploy plugnrent latest"
+heroku container:push --app=protected-escarpment-21267 web
+heroku container:release --app=protected-escarpment-21267 web
