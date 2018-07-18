de4d348d6776236a2d18bd76d3a5703fa8ec97f6
Tyler Smith <tyler.smith.la@gmail.com>
2018-07-17 21:49:21 -0400

    Fixed a range of issues
    Added simple style to app register, updated from apollo-codegen to apollo-cli for generating types, added @plugnrent/controller to client build, updated client tslint to eliminate silly errors

--- packages/app/src/modules/register/RegisterConnector.tsx
+++ packages/app/src/modules/register/RegisterConnector.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
+import { RegisterController } from "@plugnrent/controller";
 import { RegisterView } from "./view/RegisterView";
 
 export class RegisterConnector extends React.PureComponent {
-  dummySubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
-
   render() {
-    return <RegisterView submit={this.dummySubmit} />;
+    return (
+      <RegisterController>
+        {({ submit }) => <RegisterView submit={submit} />}
+      </RegisterController>
+    );
   }
 }
--- packages/app/src/modules/register/view/RegisterView.tsx
+++ packages/app/src/modules/register/view/RegisterView.tsx
@@ -1,13 +1,8 @@
 import * as React from "react";
-import {
-  withFormik,
-  FormikErrors,
-  FormikValues,
-  FormikProps,
-  Field
-} from "formik";
-import { View, Button } from "react-native";
+import { withFormik, FormikErrors, FormikProps, Field } from "formik";
 import { validUserSchema } from "@plugnrent/common";
+import { View, Text } from "react-native";
+import { Card, Button } from "react-native-elements";
 import { InputField } from "../../shared/InputField";
 
 interface FormValues {
@@ -16,24 +11,43 @@ interface FormValues {
 }
 
 interface Props {
-  submit: (values: FormValues) => Promise<FormikErrors<FormikValues> | null>;
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
 }
 
 class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <View style={{ marginTop: 200 }}>
-        <Field name="email" placeholder="E-mail" component={InputField} />
-
-        <Field
-          name="password"
-          secureTextEntry={true}
-          placeholder="Password"
-          component={InputField}
-        />
-
-        <Button title="submit" onPress={handleSubmit as any} />
+      <View
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
+        <Card>
+          <Text style={{ fontSize: 30, marginBottom: 10 }}>Register</Text>
+          <Field
+            name="email"
+            placeholder="Email"
+            component={InputField}
+            containerStyle={{ width: "100%" }}
+            autoCapitalize="none"
+          />
+          <Field
+            name="password"
+            secureTextEntry={true}
+            placeholder="Password"
+            component={InputField}
+            containerStyle={{ width: "100%" }}
+            autoCapitalize="none"
+          />
+          <Button
+            style={{ marginTop: 30 }}
+            title="Submit"
+            onPress={handleSubmit as any}
+          />
+        </Card>
       </View>
     );
   }
@@ -44,7 +58,6 @@ export const RegisterView = withFormik<Props, FormValues>({
   mapPropsToValues: () => ({ email: "", password: "" }),
   handleSubmit: async (values, { props, setErrors }) => {
     const errors = await props.submit(values);
-
     if (errors) {
       setErrors(errors);
     }
